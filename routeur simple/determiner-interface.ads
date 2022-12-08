012f0d9330f88274a2e7fcc898a9485b5e616ffe
AUCHERENathan <nathan.auchere@etu.toulouse-inp.fr>
2022-12-08 16:38:44 +0100

    initialiser_table et correctif IP.ads

--- routeur simple/determiner-interface.ads
+++
@@ -1,3 +0,0 @@
-function Determiner_Interface(IP: in T_IP;Table_Routage: in T_TABLE ) return String
-    with pre => non Est_Vide(Table_Routage)
-
--- routeur simple/ecrire-resultat.ads
+++
@@ -1,5 +0,0 @@
-procedure Ecrire_Resultat(IP: in T_IP;Interface: in String;Fichier: in out File_Type) is
-    with Pre => Est_Present(Fichier)
-         Post => Est_Ecrit(IP, Interface, Fichier)
-
-
