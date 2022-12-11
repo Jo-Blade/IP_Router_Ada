7068a4c700e6e163ef7f0b6d5ca789dec8b8f788
pisentt <teo.pisenti@free.fr>
2022-12-11 20:24:01 +0100

    mise à jour de split_exemple.adb avec le cas d’une adresse ip

--- Routeur_Simple/split_exemple.adb
+++ Routeur_Simple/split_exemple.adb
@@ -2,16 +2,23 @@ with Ada.Strings.Unbounded;    use Ada.Strings.Unbounded;
 with Ada.Text_IO;              use Ada.Text_IO;
 with Str_Split;
 
+with Str2Int;                  use Str2Int;
+with Ada.Integer_Text_IO;      use Ada.Integer_Text_IO;
+
 procedure split_exemple is
   exemple_chaine : constant String := "ceci est un test";
   exemple_chaine_error : constant String := "ceci est test";
   exemple_chaine_error2 : constant String := "ceci est un mauvais test";
+
+  exemple_chaine_ip : constant String := "192.168.1.200";
 
   package Split4 is
     new Str_Split (NbrArgs => 4);
   use Split4;
 
   tab : T_TAB;
+
+  n : Integer;
 begin
   Split(tab, To_Unbounded_String(exemple_chaine), ' ');
 
@@ -35,5 +42,14 @@ begin
       New_Line;
       Put("la chaine est erronée, trop de mots");
   end;
+
+  Split(tab, To_Unbounded_String(exemple_chaine_ip), '.');
+
+  for i in 1..4 loop
+    n := Texte_Vers_Entier( To_String(tab(i)) );
+
+    New_Line;
+    Put( n );
+  end loop;
 
 end split_exemple;
