0b0e31a9b22054fde923f63578caddc20e8f161b
Charles Ines <icharles@lafontaine.enseeiht.fr>
2022-12-12 18:36:31 +0100

    test_ip

--- Routeur_Simple/test_ip.adb
+++ Routeur_Simple/test_ip.adb
@@ -2,28 +2,24 @@ with Ada.Strings.Unbounded;    use Ada.Strings.Unbounded;
 with Ada.Text_IO;      use Ada.Text_IO;
 with Ada.Integer_Text_IO;      use Ada.Integer_Text_IO;
 with IP;      use IP;
+with My_Strings;         use My_Strings;
 
 procedure test_ip is
     IP1 : T_IP;
     IP2 : T_IP;
     IP3 : T_IP;
-    IP4 : T_IP;
-    IP5 : T_IP;
+    Nb: T_IP;
     Masque1 : T_IP;
     Masque2 : T_IP;
     IP_Entier : T_IP;
+    txt1 : Unbounded_String;
+    txt2 : Unbounded_String;
 begin
-    Texte_Vers_IP(IP1, To_Unbounded_String("192.168.0.20"));
-
-    Put(To_String(IP_Vers_Texte(IP1)));
-
-    for i in 1..32 loop
-        New_Line;
-        Put( Lire_Bit(IP1, i) );
-    end loop;
     
+
    
     -- Tests Egalite_IP
+    Texte_Vers_IP(IP1, To_Unbounded_String("192.168.0.20"));
     Texte_Vers_IP(IP2, To_Unbounded_String("192.168.0.20"));
     Texte_Vers_IP(IP3, To_Unbounded_String("192.168.1.20"));
     Texte_Vers_IP(Masque1, To_Unbounded_String("255.255.255.255"));
@@ -34,19 +30,26 @@ begin
     pragma Assert(Egalite_IP(IP1,IP3,Masque2));
     
     --Tests Lire_Bit
+    Texte_Vers_IP(Nb,To_Unbounded_String("255.0.0.0"));
+    for i in 1..8 loop
+        Pragma Assert( Lire_Bit(Nb, i) = 1);
+    end loop;
+    for i in 9..32 loop
+        Pragma Assert( Lire_Bit(Nb, i) = 0);
+    end loop;
     
     -- Tests Texte_Vers_IP et IP_Vers_Texte
     Texte_Vers_IP(IP2, To_Unbounded_String("192.168.0.20"));
-    IP4:= IP_Vers_Texte(IP2);
+    txt1 := IP_Vers_Texte(IP2);
     Texte_Vers_IP(IP3, To_Unbounded_String("192.168.1.20"));
-    IP5:= IP_Vers_Texte(IP3);
-    Pragma Assert(IP4 = To_Unbounded_String("192.168.0.20"));
-    Pragma Assert(IP5 = To_Unbounded_String("192.168.1.20"));    
+    txt2 := IP_Vers_Texte(IP3);
+    Pragma Assert(txt1 = To_Unbounded_String("192.168.0.20"));
+    Pragma Assert(txt2 = To_Unbounded_String("192.168.1.20"));    
     begin 
         Texte_Vers_IP(IP2, To_Unbounded_String("192.168.a.20"));
     exception
-        when Erreur_Nombre_Arguments =>
-            raise Erreur_Chaine_Non_IP;
+        when others=>
+            put(" Erreur_Chaine_Non_IP levée");
     end;
     
     -- tests Entier_Vers_IP
