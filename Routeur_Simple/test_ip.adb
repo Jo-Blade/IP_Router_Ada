86d397ec3d0b85234a47b1f60fcc6ef2e1638e53
Charles Ines <icharles@lafontaine.enseeiht.fr>
2022-12-12 15:58:16 +0100

    test_ip

--- Routeur_Simple/test_ip.adb
+++ Routeur_Simple/test_ip.adb
@@ -7,6 +7,8 @@ procedure test_ip is
     IP1 : T_IP;
     IP2 : T_IP;
     IP3 : T_IP;
+    IP4 : T_IP;
+    IP5 : T_IP;
     Masque1 : T_IP;
     Masque2 : T_IP;
     IP_Entier : T_IP;
@@ -20,7 +22,7 @@ begin
         Put( Lire_Bit(IP1, i) );
     end loop;
     
-
+   
     -- Tests Egalite_IP
     Texte_Vers_IP(IP2, To_Unbounded_String("192.168.0.20"));
     Texte_Vers_IP(IP3, To_Unbounded_String("192.168.1.20"));
@@ -30,8 +32,23 @@ begin
     pragma Assert(Egalite_IP(IP1,IP2,Masque2));
     pragma Assert(not Egalite_IP(IP1,IP3,Masque1));
     pragma Assert(Egalite_IP(IP1,IP3,Masque2));
-
-
+    
+    --Tests Lire_Bit
+    
+    -- Tests Texte_Vers_IP et IP_Vers_Texte
+    Texte_Vers_IP(IP2, To_Unbounded_String("192.168.0.20"));
+    IP4:= IP_Vers_Texte(IP2);
+    Texte_Vers_IP(IP3, To_Unbounded_String("192.168.1.20"));
+    IP5:= IP_Vers_Texte(IP3);
+    Pragma Assert(IP4 = To_Unbounded_String("192.168.0.20"));
+    Pragma Assert(IP5 = To_Unbounded_String("192.168.1.20"));    
+    begin 
+        Texte_Vers_IP(IP2, To_Unbounded_String("192.168.a.20"));
+    exception
+        when Erreur_Nombre_Arguments =>
+            raise Erreur_Chaine_Non_IP;
+    end;
+    
     -- tests Entier_Vers_IP
     Entier_Vers_IP(IP_Entier, 6);
     Texte_Vers_IP(IP2, To_Unbounded_String("0.0.0.6"));
