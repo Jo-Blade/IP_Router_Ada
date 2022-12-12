ad66da34213459e54f1c0f21210655e0fd82e80c
AUCHERENathan <nathan.auchere@etu.toulouse-inp.fr>
2022-12-12 12:18:51 +0100

    lire_param n'est pas un sous-programme

--- Routeur_Simple/lire_parametres.ads
+++
@@ -1,3 +0,0 @@
---Lire les paramètres d'execution: Table, Cache, Stat, Fin.--
-procedure Lire_Parametre(Commande: out String) with
-        Pre => Commande = "Table" or Commande = "Cache" or Commande = "Stat" or Commande = "Fin";
