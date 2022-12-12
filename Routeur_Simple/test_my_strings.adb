e2ae4c6a36cb0c6925b1b95d16049139bafc1765
AUCHERE Nathan <nathan.auchere@etu.toulouse-inp.fr>
2022-12-12 15:48:50 +0100

    test_my_strings.add de carac_vers_entier, texte_vers_entier et entier_pos_vers_texte

--- Routeur_Simple/test_my_strings.adb
+++ Routeur_Simple/test_my_strings.adb
@@ -3,21 +3,35 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure test_my_strings is
+    Entier : Integer;
 begin
-  Put(Caractere_Vers_Entier('8'));
-  New_Line;
-  Put(Texte_Vers_Entier("132"));
-  New_Line;
-  Put(Texte_Vers_Entier("+ 132"));
-  New_Line;
-  Put(Texte_Vers_Entier("- 823"));
-  New_Line;
+    -- Test Caractere_Vers_Entier
+    pragma Assert(Caractere_Vers_Entier('8')=8);
+    
 
-  begin
-    Put(Texte_Vers_Entier("8 - 23"));
-  exception
-    when Erreur_Pas_Un_Entier =>
-      Put("8 - 23 n’est pas une chaine valide");
-  end;
+    -- Tests Texte_Vers_Entier
+    pragma Assert(Texte_Vers_Entier("8") = 8);
+    pragma Assert(Texte_Vers_Entier("132")=132);
+    pragma Assert(Texte_Vers_Entier("+126")=126);
+    pragma Assert(Texte_Vers_Entier("- 26")=-26);
+    begin
+        Entier := Texte_Vers_Entier("8 - 23");  -- Doit lever l'exception Erreur_Pas_Un_Entier
+    exception
+        when Erreur_Pas_Un_Entier =>
+            Put("8 - 23 n’est pas une chaine valide");
+    end;
+
+    
+    -- Tests Entier_Positif_Vers_Texte
+    pragma Assert(Entier_Positif_Vers_Texte(124)="124");
+    pragma Assert(Entier_Positif_Vers_Texte(0)="0");
+
+
+    Put_Line("");
+    Put_Line("");
+    Put_Line("##################################################");
+    Put_Line("#################### ALL OK ! ####################");
+    Put_Line("##################################################");
+    Put_Line("");
 
 end test_my_strings;
