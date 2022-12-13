340b14969b36be95b969d695817703756f484337
AUCHERENathan <nathan.auchere@etu.toulouse-inp.fr>
2022-12-13 13:54:22 +0100

    écriture des grandes lignes de routage.adb et petits correctifs sur routage.ads

---
+++ Routeur_Simple/routage.adb
@@ -0,0 +1,29 @@
+with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
+
+package body Routage is
+    
+    -- function Initialiser_Table (Fichier : in File_Type) return T_LC is
+    --     Table_Routage : T_LC;
+    -- begin
+    --     return Table_Routage;
+    -- end Initialiser_Table;
+
+
+    function Trouver_Interface (Table_Routage : in T_LC; IP : in T_IP) return Unbounded_String is
+    begin
+        return To_Unbounded_String("");
+        -- Instanciation de Pour_Chaque avec Traiter qui prend l'ip et l'élément courant de Table_Routage
+        -- On crée une variable """globale""" qui prendra la longueure maximale de masque rencontré (et valide)
+        -- Idem pour une variable qui stockera le nom de l'interface
+        -- On passe pour chaque élément dans traiter la condition de validité du masque et s'il passe,
+        -- on stocke sa longueure et l'interface qui lui est associée
+        -- Après le passage de Pour_Chaque, on renvoie l'interface
+    end Trouver_Interface;
+
+
+    -- procedure Afficher_Table (Table_Routage : in T_LC) is
+    -- begin
+    --     null;
+    -- end Afficher_Table;
+
+end Routage;
--- Routeur_Simple/routage.ads
+++ Routeur_Simple/routage.ads
@@ -1,28 +1,29 @@
+with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
 with Liste_Chainee;
 
 package Routage is
 
-type T_Routage_Valeur is record
-    Destination : Integer;
-    Masque : Integer;
-    Interface_Nom : String;
-    end record;
+    type T_Routage_Valeur is record
+        Destination : Integer;
+        Masque : Integer;
+        Interface_Nom : String;
+        end record;
 
 
-    package Table_Routage is new Liste_Chainee(T_Element => T_Routage_Valeur)
+    package Table_Routage is new Liste_Chainee(T_Element => T_Routage_Valeur);
 
--- Initialiser la table de routage avec le fichier dédié
-function Initialiser_Table (Fichier : in File_Type) return T_LC with
-    Pre => Est_Present (Fichier);
-    Post => not Est_Vide (Table)
-        and Contient (Table, Texte_Vers_IP("0.0.0.0"), "eth0");
-
-
-
-
-
-function Trouver_Interface(Table_Routage: T_LC ; IP: T_IP) return Unbounded_String;
-
-
-
+    -- Initialiser la table de routage avec le fichier dédié
+    function Initialiser_Table (Fichier : in File_Type) return T_LC with
+        Pre => Est_Present (Fichier)
+        Post => not Est_Vide (Table)
+            and Contient (Table, Texte_Vers_IP("0.0.0.0"), "eth0");
+    
+    
+    function Trouver_Interface(Table_Routage : in T_LC ; IP : in T_IP) return Unbounded_String;
+    
+    
+    
+    procedure Afficher_Table(Table_Routage: in T_LC);
+    
+    
 end Routage;
