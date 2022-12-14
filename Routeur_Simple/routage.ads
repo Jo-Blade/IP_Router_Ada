c42c97c2fd359746fefd7daecac8b7bceed96452
pisentt <teo.pisenti@free.fr>
2022-12-14 20:48:37 +0100

    je sais pas ce que j’ai fait comme betise, mais routage.ads est de retour

--- Routeur_Simple/routage.ads
+++ Routeur_Simple/routage.ads
@@ -12,35 +12,32 @@ package Routage is
   -- Initialiser la table de routage avec le fichier dédié
   procedure Initialiser_Table (Table_Routage : out T_Table; Fichier : in File_Type) with
     Pre => Is_Open(Fichier),
-    Post => not Est_Vide (Table_Routage)
-    and Contient_IP_Nulle (Table_Routage);
+    Post => not Est_Vide (Table_Routage);
+    -- and Contient_IP_Nulle (Table_Routage);
 
     function Trouver_Interface(Table_Routage : in T_Table ; IP : in T_IP) return Unbounded_String;
 
     --Afficher tous les élément de la table de routage
-    procedure Afficher_Table(Table_Routage: in T_Table);
+    -- procedure Afficher_Table(Table_Routage: in T_Table);
 
-    function Contient_IP_Nulle(Table_Routage : in T_Table) return Boolean;
+    -- function Contient_IP_Nulle(Table_Routage : in T_Table) return Boolean;
 
     function Est_Vide (Table_Routage : in T_Table) return Boolean;
 
+	-- Appliquer un traitement (Traiter) pour chaque élement d'une Liste.
+    generic
+      with procedure Traiter (Adresse : in T_IP; Masque : in T_IP; Interface_Nom : in Unbounded_String);
+    procedure Pour_Chaque (Table_Routage : in T_Table);
+
+
 private
 
-<<<<<<< HEAD
-    type T_Cellule is limited
-      record
-        Adresse : T_IP;
-        Masque : T_IP;
-        Interface_Nom : Unbounded_String;
-      end record;
-=======
     type T_Cellule is
         record
             Adresse : T_IP;
             Masque : T_IP;
             Interface_Nom : Unbounded_String;
         end record;
->>>>>>> refs/remotes/origin/main
 
     package Table_LC is new Liste_Chainee(T_Element => T_Cellule);
     use Table_LC;
