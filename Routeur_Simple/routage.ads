4f9b5541a0fc2471cd4597126fb147aeb840021d
AUCHERENathan <nathan.auchere@etu.toulouse-inp.fr>
2022-12-14 19:27:24 +0100

    Contient_IP_NUlle de routage.ads et avancement divers sur routage .adb

--- Routeur_Simple/routage.ads
+++ Routeur_Simple/routage.ads
@@ -5,41 +5,36 @@ with Liste_Chainee;
 
 package Routage is
 
-  type T_Table is limited private;
+    type T_Table is limited private;
 
 
-  -- Initialiser la table de routage avec le fichier dédié
-  procedure Initialiser_Table (Table_Routage : out T_Table; Fichier : in File_Type) with
-  Pre => Is_Open(Fichier),
-  Post => not Est_Vide (Table_Routage);
---  and Contient (Table_Routage, Texte_Vers_IP(To_Unbounded_String("0.0.0.0")), Texte_Vers_IP("0.0.0.0"), "eth0");
+    -- Initialiser la table de routage avec le fichier dédié
+    procedure Initialiser_Table (Table_Routage : out T_Table; Fichier : in File_Type) with
+    Pre => Is_Open(Fichier),
+    Post => not Est_Vide (Table_Routage)
+        and Contient_IP_Nulle (Table_Routage);
 
-  function Trouver_Interface(Table_Routage : in T_Table ; IP : in T_IP) return Unbounded_String;
+    function Trouver_Interface(Table_Routage : in T_Table ; IP : in T_IP) return Unbounded_String;
 
-  --Afficher tous les élément de la table de routage
-  procedure Afficher_Table(Table_Routage: in T_Table);
+    --Afficher tous les élément de la table de routage
+    procedure Afficher_Table(Table_Routage: in T_Table);
 
-  function Contient(Table_Routage : in T_Table; Adresse : in T_IP) return Boolean;
+    function Contient_IP_Nulle(Table_Routage : in T_Table) return Boolean;
 
-  function Contient(Table_Routage : in T_Table; Adresse : in T_IP; Masque : in T_IP) return Boolean;
+    function Est_Vide (Table_Routage : in T_Table) return Boolean;
 
-  function Contient(Table_Routage : in T_Table; Adresse : in T_IP; Masque : in T_IP;
-    Interface_Nom : in Unbounded_String) return Boolean;
+    private
 
-	function Est_Vide (Table_Routage : in T_Table) return Boolean;
+    type T_Cellule is
+        record
+            Adresse : Integer;
+            Masque : Integer;
+            Interface_Nom : Unbounded_String;
+        end record;
 
-  private
+    package Table_LC is new Liste_Chainee(T_Element => T_Cellule);
+    use Table_LC;
 
-  type T_Cellule is
-    record
-      Adresse : Integer;
-      Masque : Integer;
-      Interface_Nom : Unbounded_String;
-    end record;
-
-  package Table_LC is new Liste_Chainee(T_Element => T_Cellule);
-  use Table_LC;
-
-  type T_Table is new T_LC;
+    type T_Table is new T_LC;
 
 end Routage;
