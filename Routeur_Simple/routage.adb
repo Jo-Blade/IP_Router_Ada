ada01da00bb7385a3d1053c36243f846c752f935
pisentt <teo.pisenti@free.fr>
2022-12-14 21:22:07 +0100

    ajout des fonctions Pour_Chaque_[1-2-3] pour faire disparaitre les warnings de compilation

--- Routeur_Simple/routage.adb
+++ Routeur_Simple/routage.adb
@@ -1,125 +1,182 @@
-with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
-with Ada.Text_IO;               use Ada.Text_IO;
-with IP;                        use IP;
-with Ada.Exceptions;            use Ada.Exceptions;
-with Liste_Chainee;
-with Str_Split;
-
 package body Routage is
     
   procedure Afficher_Table(Table_Routage: in T_Table) is
     procedure Afficher (Adresse : in T_IP; Masque : in T_IP; Interface_Nom : in Unbounded_String) is
     begin
-        Put("A MODIFIER");
-        New_Line;
+      Put_Line(To_String(IP_Vers_Texte(Adresse) & IP_Vers_Texte(Masque) & Interface_Nom));
     end Afficher;
 
-    procedure Afficher_Table_Bis is new Pour_Chaque (Traiter => Afficher);
+    procedure Afficher_Table_Bis is new Pour_Chaque_3 (Traiter => Afficher);
   begin
     Afficher_Table_Bis (Table_Routage);
   end Afficher_Table;
-    
-
---     function Contient_IP_Nulle(Table_Routage : in T_Table) return Boolean is
---         Est_Present : Boolean;
---         Adresse_Nulle : T_IP;
---         Masque_Nul : T_IP;
---         Masque_Parfait : T_IP;
---         -- Définition de la procedure Trouver qui s'appliquera pour chaque élément de la table de routage
---         procedure Trouver (Element : T_Cellule) is
---         begin
---             if Egalite_IP(Adresse_Nulle, Element.Adresse, Masque_Parfait) and Egalite_IP(Masque_Nul, Element.Masque, Masque_Parfait) then
---                 Est_Present := True;
---             else
---                 Null;
---             end if;
---         end Trouver;
--- 
---         procedure Pour_Chaque_Element is new Table_LC.Pour_Chaque (Traiter => Trouver);
---     begin
---         Est_Present := False;
---         Texte_Vers_IP(Adresse_Nulle, To_Unbounded_String("0.0.0.0"));
---         Texte_Vers_IP(Masque_Nul, To_Unbounded_String("0.0.0.0"));
---         Texte_Vers_IP(Masque_Parfait, To_Unbounded_String("255.255.255.255"));
---         Pour_Chaque_Element (T_LC(Table_Routage));
---         return Est_Present;
---     end Contient_IP_Nulle;
 
 
-    function Est_Vide (Table_Routage : in T_Table) return Boolean is
+
+  function Contient (Table_Routage : in T_Table; Adresse : in T_IP) return Boolean is
+    Resultat_Test : Boolean := False;
+
+    procedure Traiter (Adresse_El : in T_IP) is
     begin
-        return Table_LC.Est_Vide(T_LC(Table_Routage));
-    end Est_Vide;
+      if Adresse = Adresse_El then
+        Resultat_Test := True;
+      else
+        Null;
+      end if;
+    end Traiter;
 
-    procedure Initialiser_Table (Table_Routage : out T_Table; Fichier : in File_Type) is
+    procedure Contient_Bis is new Pour_Chaque_1 (Traiter => Traiter);
+
+  begin
+    Contient_Bis (Table_Routage);
+    return Resultat_Test;
+  end Contient;
+
+
+
+  function Contient (Table_Routage : in T_Table; Adresse : in T_IP; Masque : in T_IP) return Boolean is
+    Resultat_Test : Boolean := False;
+
+    procedure Traiter (Adresse_El : in T_IP; Masque_El : in T_IP) is
     begin
-         Table_LC.Initialiser(T_LC(Table_Routage));
-    end Initialiser_Table; 
+      if (Adresse = Adresse_El) and (Masque = Masque_El) then
+        Resultat_Test := True;
+      else
+        Null;
+      end if;
+    end Traiter;
+
+    procedure Contient_Bis is new Pour_Chaque_2 (Traiter => Traiter);
+
+  begin
+    Contient_Bis (Table_Routage);
+    return Resultat_Test;
+  end Contient;
 
 
-    function Trouver_Interface (Table_Routage : in T_Table; IP : in T_IP) return Unbounded_String is
-        -- Exception
-        Interface_Par_Defaut : Exception;
 
-        -- Variables locales
-        Longueur_Max : Integer;              -- Plus grande longueur de masque
-        Interface_Trouve : Unbounded_String;    -- Nom de l'interface vers laquelle sera routé le paquet, "eth0" de base
+  function Contient (Table_Routage : in T_Table; Adresse : in T_IP; Masque : in T_IP;
+    Interface_Nom : Unbounded_String) return Boolean is
+    Resultat_Test : Boolean := False;
 
-        -- Définition de la procedure Trouver qui s'appliquera pour chaque élément de la table de routage
-        procedure Trouver (Adresse : in T_IP; Masque : in T_IP; Interface_Nom : in Unbounded_String) is
-            Taille_Masque : Integer;    -- Taille du masque courant
-        begin
-            Taille_Masque := Longueur_IP(Masque);
-            if Egalite_IP(IP, Adresse, Masque) and then (Taille_Masque > Longueur_Max) then
-                Longueur_Max := Taille_Masque;
-                Interface_Trouve := Interface_Nom;
-            else
-                Null;
-            end if;
-            if Longueur_Max = 0 then
-                raise Interface_Par_Defaut;
-            else
-                Null;
-            end if;
-        end Trouver;
-
-        procedure Pour_Chaque_Interface is new Pour_Chaque (Traiter => Trouver);
-
+    procedure Traiter (Adresse_El : in T_IP; Masque_El : in T_IP; Interface_Nom_El : in Unbounded_String) is
     begin
-        -- Instanciation de Pour_Chaque avec Traiter qui prend l'ip et l'élément courant de Table_Routage
-        -- On crée une variable """globale""" qui prendra la longueure maximale de masque rencontré (et valide)
-        -- Idem pour une variable qui stockera le nom de l'interface
-        -- On passe pour chaque élément dans traiter la condition de validité du masque et s'il passe,
-        -- on stocke sa longueure et l'interface qui lui est associée
-        -- Après le passage de Pour_Chaque, on renvoie l'interface
-        --
-        -- FAUDRA PAS OUBLIER DE RENVOYER UNE EXCEPTION SI ON NE TROUVE PAS D’INTERFACE (ce qui est censé être
-        -- impossible dans le cas du routeur simple car la table contient toujours 0.0.0.0 mais ça sera utile
-        -- pour le routeur avec cache)
-        --
-        -- il faut prendre en compte l’exception Erreur_Masque_Invalide levée dans Longueur_IP
-        -- dans le code de la fonction Traiter 
-        -- en affichant un message d’erreur: 
-        -- "Le masque <adresse_du_masque> n’est pas un masque valide, l’interface <nom_interface> sera ignorée"
-        -- et ignorer cette ligne de la table
-        Longueur_Max := 0;
-        Interface_Trouve := To_Unbounded_String("eth0");
-        Pour_Chaque_Interface (Table_Routage);
-        return Interface_Trouve;
-    end Trouver_Interface;
+      if (Adresse = Adresse_El) and (Masque = Masque_El) and (Interface_Nom = Interface_Nom_El) then
+        Resultat_Test := True;
+      else
+        Null;
+      end if;
+    end Traiter;
+
+    procedure Contient_Bis is new Pour_Chaque_3 (Traiter => Traiter);
+
+  begin
+    Contient_Bis (Table_Routage);
+    return Resultat_Test;
+  end Contient;
 
 
 
-    procedure Pour_Chaque(Table_Routage : in T_Table) is
-      procedure Traiter_LC (Cellule : T_Cellule) is
-      begin
-        Traiter(Cellule.Adresse, Cellule.Masque, Cellule.Interface_Nom);
-      end Traiter_LC;
+  function Est_Vide (Table_Routage : in T_Table) return Boolean is
+  begin
+    return Table_LC.Est_Vide(T_LC(Table_Routage));
+  end Est_Vide;
 
-      procedure Pour_Chaque_LC is new Table_LC.Pour_Chaque(Traiter => Traiter_LC);
+  procedure Initialiser_Table (Table_Routage : out T_Table; Fichier : in File_Type) is
+  begin
+    Table_LC.Initialiser(T_LC(Table_Routage));
+  end Initialiser_Table; 
+
+
+  function Trouver_Interface (Table_Routage : in T_Table; IP : in T_IP) return Unbounded_String is
+    -- Exception
+    Interface_Par_Defaut : Exception;
+
+    -- Variables locales
+    Longueur_Max : Integer;              -- Plus grande longueur de masque
+    Interface_Trouve : Unbounded_String;    -- Nom de l'interface vers laquelle sera routé le paquet, "eth0" de base
+
+    -- Définition de la procedure Trouver qui s'appliquera pour chaque élément de la table de routage
+    procedure Trouver (Adresse : in T_IP; Masque : in T_IP; Interface_Nom : in Unbounded_String) is
+      Taille_Masque : Integer;    -- Taille du masque courant
     begin
-      Pour_Chaque_LC(T_LC(Table_Routage));
-    end Pour_Chaque;
+      Taille_Masque := Longueur_IP(Masque);
+      if Egalite_IP(IP, Adresse, Masque) and then (Taille_Masque > Longueur_Max) then
+        Longueur_Max := Taille_Masque;
+        Interface_Trouve := Interface_Nom;
+      else
+        Null;
+      end if;
+      if Longueur_Max = 0 then
+        raise Interface_Par_Defaut;
+      else
+        Null;
+      end if;
+    end Trouver;
+
+    procedure Pour_Chaque_Interface is new Pour_Chaque_3 (Traiter => Trouver);
+
+  begin
+    -- Instanciation de Pour_Chaque avec Traiter qui prend l'ip et l'élément courant de Table_Routage
+    -- On crée une variable """globale""" qui prendra la longueure maximale de masque rencontré (et valide)
+    -- Idem pour une variable qui stockera le nom de l'interface
+    -- On passe pour chaque élément dans traiter la condition de validité du masque et s'il passe,
+    -- on stocke sa longueure et l'interface qui lui est associée
+    -- Après le passage de Pour_Chaque, on renvoie l'interface
+    --
+    -- FAUDRA PAS OUBLIER DE RENVOYER UNE EXCEPTION SI ON NE TROUVE PAS D’INTERFACE (ce qui est censé être
+    -- impossible dans le cas du routeur simple car la table contient toujours 0.0.0.0 mais ça sera utile
+    -- pour le routeur avec cache)
+    --
+    -- il faut prendre en compte l’exception Erreur_Masque_Invalide levée dans Longueur_IP
+    -- dans le code de la fonction Traiter 
+    -- en affichant un message d’erreur: 
+    -- "Le masque <adresse_du_masque> n’est pas un masque valide, l’interface <nom_interface> sera ignorée"
+    -- et ignorer cette ligne de la table
+    Longueur_Max := 0;
+    Interface_Trouve := To_Unbounded_String("eth0");
+    Pour_Chaque_Interface (Table_Routage);
+    return Interface_Trouve;
+  end Trouver_Interface;
+
+
+
+  procedure Pour_Chaque_1 (Table_Routage : in T_Table) is
+    procedure Traiter_LC (Cellule : T_Cellule) is
+    begin
+      Traiter(Cellule.Adresse);
+    end Traiter_LC;
+
+    procedure Pour_Chaque_LC is new Table_LC.Pour_Chaque(Traiter => Traiter_LC);
+  begin
+    Pour_Chaque_LC(T_LC(Table_Routage));
+  end Pour_Chaque_1;
+
+
+
+  procedure Pour_Chaque_2 (Table_Routage : in T_Table) is
+    procedure Traiter_LC (Cellule : T_Cellule) is
+    begin
+      Traiter(Cellule.Adresse, Cellule.Masque);
+    end Traiter_LC;
+
+    procedure Pour_Chaque_LC is new Table_LC.Pour_Chaque(Traiter => Traiter_LC);
+  begin
+    Pour_Chaque_LC(T_LC(Table_Routage));
+  end Pour_Chaque_2;
+
+
+
+  procedure Pour_Chaque_3 (Table_Routage : in T_Table) is
+    procedure Traiter_LC (Cellule : T_Cellule) is
+    begin
+      Traiter(Cellule.Adresse, Cellule.Masque, Cellule.Interface_Nom);
+    end Traiter_LC;
+
+    procedure Pour_Chaque_LC is new Table_LC.Pour_Chaque(Traiter => Traiter_LC);
+  begin
+    Pour_Chaque_LC(T_LC(Table_Routage));
+  end Pour_Chaque_3;
 
 
 end Routage;
--- Routeur_Simple/routage.ads
+++ Routeur_Simple/routage.ads
@@ -12,37 +12,54 @@ package Routage is
   -- Initialiser la table de routage avec le fichier dédié
   procedure Initialiser_Table (Table_Routage : out T_Table; Fichier : in File_Type) with
     Pre => Is_Open(Fichier),
-    Post => not Est_Vide (Table_Routage);
-    -- and Contient_IP_Nulle (Table_Routage);
+    Post => not Est_Vide (Table_Routage)
+    and Contient(Table_Routage, Texte_Vers_IP(To_Unbounded_String("0.0.0.0"))
+    , Texte_Vers_IP(To_Unbounded_String("0.0.0.0")));
 
     function Trouver_Interface(Table_Routage : in T_Table ; IP : in T_IP) return Unbounded_String;
 
     --Afficher tous les élément de la table de routage
-    -- procedure Afficher_Table(Table_Routage: in T_Table);
-
-    -- function Contient_IP_Nulle(Table_Routage : in T_Table) return Boolean;
+    procedure Afficher_Table(Table_Routage: in T_Table);
 
     function Est_Vide (Table_Routage : in T_Table) return Boolean;
 
-	-- Appliquer un traitement (Traiter) pour chaque élement d'une Liste.
+    -- Appliquer un traitement (Traiter) pour chaque élement d'une Liste.
+    -- On a 3 fonctions identiques pour éviter les warnings si un paramètre est inutilisé
+    -- (voir les fonctions Contient ci-dessous)
     generic
-      with procedure Traiter (Adresse : in T_IP; Masque : in T_IP; Interface_Nom : in Unbounded_String);
-    procedure Pour_Chaque (Table_Routage : in T_Table);
+    with procedure Traiter (Adresse : in T_IP);
+    procedure Pour_Chaque_1 (Table_Routage : in T_Table);
+
+    generic
+    with procedure Traiter (Adresse : in T_IP; Masque : in T_IP);
+    procedure Pour_Chaque_2 (Table_Routage : in T_Table);
+
+    generic
+    with procedure Traiter (Adresse : in T_IP; Masque : in T_IP; Interface_Nom : in Unbounded_String);
+    procedure Pour_Chaque_3 (Table_Routage : in T_Table);
 
 
-private
+    -- 3 fonctions qui vérifient si un certain couple est contenu au moins une fois dans la table
+    function Contient (Table_Routage : in T_Table; Adresse : in T_IP) return Boolean;
 
-    type T_Cellule is
+    function Contient (Table_Routage : in T_Table; Adresse : in T_IP; Masque : in T_IP) return Boolean;
+
+    function Contient (Table_Routage : in T_Table; Adresse : in T_IP; Masque : in T_IP;
+      Interface_Nom : in Unbounded_String ) return Boolean;
+
+      private
+
+      type T_Cellule is
         record
-            Adresse : T_IP;
-            Masque : T_IP;
-            Interface_Nom : Unbounded_String;
+          Adresse : T_IP;
+          Masque : T_IP;
+          Interface_Nom : Unbounded_String;
         end record;
 
-    package Table_LC is new Liste_Chainee(T_Element => T_Cellule);
-    use Table_LC;
+      package Table_LC is new Liste_Chainee(T_Element => T_Cellule);
+      use Table_LC;
 
-    type T_Table is new T_LC;
+      type T_Table is new T_LC;
 
 
 end Routage;
