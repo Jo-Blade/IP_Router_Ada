7ae43464892d37eff5accd8c874b76b6a8f16c96
Matthieu <matthieu.trichard@gmail.com>
2022-12-14 16:54:24 +0100

    initialiser table

--- Routeur_Simple/initialiser_table.adb
+++ Routeur_Simple/initialiser_table.adb
@@ -7,26 +7,27 @@ with Str_Split;
 
 
 function Initialiser_Table (Fichier : in File_Type) is
-    package Split3 is
-    new Str_Split (NbrArgs => 3);
+    package Split3 is new Str_Split(NbrArgs => 3);
     use Split3;
 
     package Liste_Chainee_record is new Liste_Chainee(Element => T_Routage_Valeur);
     use Liste_Chainee_record;
-    Tab : T_TAB;
-    Table : T_LC;
-    Ligne : Unbounded_String;
-    Elem : T_Routage_Valeur;
+
+    tab : T_TAB; --tableau pour la fonction texte_vers_ip
+    table : T_LC; --liste chainee pour le renvoie
+    ligne : Unbounded_String;
+    elem : T_Routage_Valeur;
 begin
-    open(Fichier, In_File, "");
+    open(Fichier, In_File, "");  --ouverture du fichier
     loop
-        Get_Line(Fichier, Ligne);
-        Split(Tab,Ligne, " ");
-        Texte_Vers_IP(Elem.Destination, Tab(1));
-        Texte_Vers_IP(Elem.Masque, Tab(2));
-        Elem.Interface_Nom := Tab(3);
-        Enregistrer(Table, Elem);
-    exit loop when End_Of_File(Fichier);
-
-
+        Get_Line(fichier, ligne);  --recupération de la ligne courante
+        Split(tab,ligne, " ");  --séparation des éléments
+        Texte_Vers_IP(elem.Destination, tab(1)); -- transformation en IP et affectation à elem
+        Texte_Vers_IP(elem.Masque, tab(2));
+        Elem.Interface_Nom := tab(3); -- affectation de l'interface dans la dernière case de elem
+        Enregistrer(table, tlem); --enregistrement de elem danss la table
+        ligne := ligne + 1;
+    exit when End_Of_File(fichier);
+    end loop;
+    return table;
 end Initialiser_Table;
