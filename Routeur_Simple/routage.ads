79108e235c9d04a690a7e7b013f0150266ed50a7
pisentt <teo.pisenti@free.fr>
2022-12-14 17:50:27 +0100

    premier jet (qui compile) de routage.ads

--- Routeur_Simple/routage.ads
+++ Routeur_Simple/routage.ads
@@ -1,32 +1,40 @@
+with Ada.Text_IO;               use Ada.Text_IO;
 with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
+with IP;                        use IP;
 with Liste_Chainee;
 
 package Routage is
 
-  -- CE TYPE DEVRAIT ÊTRE DÉFINI EN LIMITED PRIVATE
-    type T_Routage_Valeur is record
+  type T_Table is limited private;
+
+
+  -- Initialiser la table de routage avec le fichier dédié
+  function Initialiser_Table (Fichier : in File_Type) return T_Table; -- with
+    --Pre => Est_Present (Fichier),
+    --Post => not Est_Vide (Table)
+    --and Contient (Table, Texte_Vers_IP("0.0.0.0"), "eth0");
+
+
+    function Trouver_Interface(Table_Routage : in T_Table ; IP : in T_IP) return Unbounded_String;
+
+
+
+    --Afficher tous les élément de la table de routage
+    procedure Afficher_Table(Table_Routage: in T_Table);
+
+
+    private
+
+    type T_Cellule is
+      record
         Adresse : Integer;
         Masque : Integer;
-        Interface_Nom : String;
-        end record;
+        Interface_Nom : Unbounded_String;
+      end record;
 
+    package Table_Routage is new Liste_Chainee(T_Element => T_Cellule);
+    use Table_Routage;
 
-    -- CE PACKAGE DEVRAIT ÊTRE INSTANTIÉ DANS LE BLOC PRIVATE
-    package Table_Routage is new Liste_Chainee(T_Element => T_Routage_Valeur);
+    type T_Table is new T_LC;
 
-    -- Initialiser la table de routage avec le fichier dédié
-    function Initialiser_Table (Fichier : in File_Type) return T_LC with
-        Pre => Est_Present (Fichier)
-        Post => not Est_Vide (Table)
-            and Contient (Table, Texte_Vers_IP("0.0.0.0"), "eth0");
-    
-    
-    function Trouver_Interface(Table_Routage : in T_LC ; IP : in T_IP) return Unbounded_String;
-    
-    
-    
-    --Afficher tous les élément de la table de routage
-    procedure Afficher_Table(Table_Routage: in T_LC);
-    
-    
-end Routage;
+  end Routage;
