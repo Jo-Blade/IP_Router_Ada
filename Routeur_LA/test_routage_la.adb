538c09bb50e5629e211b82a5950e1304d4986808
Matthieu <matthieu.trichard@gmail.com>
2023-01-10 18:03:53 +0100

    Fichier test_routage_la complètement terminé

--- Routeur_LA/test_routage_la.adb
+++ Routeur_LA/test_routage_la.adb
@@ -17,34 +17,57 @@ procedure test_routage_LA is
         put("test de la procedure Mise_A_Jour_Cache"); New_Line;
 
 
-        put("print 1"); New_Line;
+        put("print Remplissage de la table de routage, Etape 1"); New_Line;
         Ajouter_Element(Table, Texte_Vers_IP(To_Unbounded_String("0.0.0.0")), Texte_Vers_IP(To_Unbounded_String("0.0.0.0")), To_Unbounded_String("eth2"));
         Afficher_Table(Table); New_Line; New_Line;
 
 
-        put("print 2"); New_Line;
+        put("Etape 2"); New_Line;
         Ajouter_Element(Table, Texte_Vers_IP(To_Unbounded_String("147.127.127.0")), Texte_Vers_IP(To_Unbounded_String("255.255.255.0")), To_Unbounded_String("eth0"));
         Afficher_Table(Table); New_Line; New_Line;
 
-        put("print 3"); New_Line;
+        put("Etape 3"); New_Line;
         Ajouter_Element(Table, Texte_Vers_IP(To_Unbounded_String("147.128.0.0")), Texte_Vers_IP(To_Unbounded_String("255.255.255.0")), To_Unbounded_String("eth1"));
         Afficher_Table(Table); New_Line; New_Line;
 
-        put("print 4"); New_Line;
+        put("Remplissage de cache"); New_Line;
+        put("Etape 1"); New_Line;
         Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.8")) ,3, To_Unbounded_String("LRU"),0,  To_Unbounded_String("eth2"), Table);
         Afficher_Table(Cache); New_Line; New_Line;
 
+        put("Etape 2"); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.128.10.10")) ,3, To_Unbounded_String("LRU"),1,  To_Unbounded_String("eth1"), Table);
+        Afficher_Table(Cache); New_Line; New_Line;
+
+        put("Etape 3"); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.5")) ,3, To_Unbounded_String("LRU"),2,  To_Unbounded_String("eth3"), Table);
+        Afficher_Table(Cache); New_Line; New_Line;
+
+
+        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.8")), To_Unbounded_String("LRU"));
+
+
+        put("Etape 4: Depassement de la taille max du cache et suppression de l'élément d'interface 'eth1' "); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.10.8")) ,3, To_Unbounded_String("LRU"),3,  To_Unbounded_String("eth3"), Table);
+        Afficher_Table(Cache); New_Line; New_Line;
+
+
+        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.128.10.10")), To_Unbounded_String("LRU"));
+
         put("Fin des tests pour Ajouter_Element."); New_Line;
+    exception
+        when INTERFACE_NON_TROUVE =>
+            put("L'élément le plus ancien du  cache a bien été supprimé");
     end test_Mise_A_Jour_Cache;
 
 
     procedure test_Trouver_Interface_Cache is
     begin
 
-
-        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.0")), To_Unbounded_String("LRU"));
+        put("Début des tests de Trouver_Interface_Cache"); New_Line;
+        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.8")), To_Unbounded_String("LRU"));
         pragma Assert(Interface_Trouvee = To_Unbounded_String("eth2"));
-        put("Trouver_Interface_Cache foncionne en entier.");
+        put("Trouver_Interface_Cache foncionne bien.");
         New_Line;
 
 
