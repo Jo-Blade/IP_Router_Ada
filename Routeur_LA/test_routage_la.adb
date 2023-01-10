91f304af2ef441652c994a7047917fe28d76ee83
Matthieu <matthieu.trichard@gmail.com>
2023-01-10 17:47:03 +0100

    test  routage la

--- Routeur_LA/test_routage_la.adb
+++ Routeur_LA/test_routage_la.adb
@@ -2,19 +2,19 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Strings.Unbounded;     use Ada.Strings.Unbounded;
 with Routage_LA; use Routage_LA;
 with IP; use IP;
+with Routage; use Routage;
 
 procedure test_routage_LA is
-    Erreur_Exception_Non_Levee : Exception;
 
-    Table : T_Table;
-    Cache : T_Table;
+    Table : Routage.T_Table;
+    Cache : Routage_LA.T_Table;
     Interface_Trouvee : Unbounded_String;
 
 
 
-    procedure test_Ajouter_Element is
+    procedure test_Mise_A_Jour_Cache is
     begin
-        put("test de la procedure Ajouter_Element"); New_Line;
+        put("test de la procedure Mise_A_Jour_Cache"); New_Line;
 
 
         put("print 1"); New_Line;
@@ -31,51 +31,25 @@ procedure test_routage_LA is
         Afficher_Table(Table); New_Line; New_Line;
 
         put("print 4"); New_Line;
-        Ajouter_Element(Cache, Texte_Vers_IP(To_Unbounded_String("147.0.0.0")), Texte_Vers_IP(To_Unbounded_String("255.192.0.0")), To_Unbounded_String("eth2"));
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.8")) ,3, To_Unbounded_String("LRU"),0,  To_Unbounded_String("eth2"), Table);
         Afficher_Table(Cache); New_Line; New_Line;
 
         put("Fin des tests pour Ajouter_Element."); New_Line;
-    end test_Ajouter_Element;
+    end test_Mise_A_Jour_Cache;
 
 
-    procedure test_Trouver_Interface_Table_Cache is
+    procedure test_Trouver_Interface_Cache is
     begin
 
 
-        Trouver_Interface_Table(Interface_Trouvee, Table, Texte_Vers_IP(To_Unbounded_String("147.5.5.5"))); New_Line;
-        pragma Assert(Interface_Trouvee = To_Unbounded_String("eth2"));
-        put("Trouver_Interface_Table fonctionne bien sans cache"); New_line;
-
-
-        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.5.5.5")));
+        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.0")), To_Unbounded_String("LRU"));
         pragma Assert(Interface_Trouvee = To_Unbounded_String("eth2"));
         put("Trouver_Interface_Cache foncionne en entier.");
         New_Line;
-    end test_Trouver_Interface_Table_Cache;
-
-    procedure test_Supprimer_Plus_Ancien is
-    begin
-
-        Put_Line("Suppression element plus ancien table");
-        Supprimer_Plus_Ancien(Table);
-        Trouver_Interface_Table(Interface_Trouvee, Table, Texte_Vers_IP(To_Unbounded_String("147.127.127.0")));
-        pragma Assert(Interface_Trouvee = To_Unbounded_String("eth2"));
-
-    end test_Supprimer_Plus_Ancien;
 
 
-    procedure test_Vider_Table is
-    begin
 
-        Put("Test de Vider_Table"); New_Line;
-        Vider_Table(Table);
-        Vider_Table(Cache);
-        pragma Assert(Est_Vide(Table));
-        pragma Assert(Est_Vide(Cache));
-        put("La procédure Vider_Table fonctionn"); New_Line;
-        put("La fonction Est_Vide fonctionne"); New_Line;
-
-    end test_Vider_Table;
+    end test_Trouver_Interface_Cache;
 
 
 begin
@@ -83,10 +57,8 @@ begin
     Initialiser_Table_Vide(Table);
     Initialiser_Table_Vide(Cache);
 
-    test_Ajouter_Element;
-    test_Trouver_Interface_Table_Cache;
-    test_Supprimer_Plus_Ancien;
-    test_Vider_Table;
+    test_Mise_A_Jour_Cache;
+    test_Trouver_Interface_Cache;
 
   New_Line;
 
