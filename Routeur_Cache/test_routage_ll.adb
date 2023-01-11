cbd23585d1faf4cd252569312fbf2af6b43ebe5e
Matthieu <matthieu.trichard@gmail.com>
2023-01-11 10:34:32 +0100

    test de la politique FIFO dans test_routage_ll

--- Routeur_Cache/test_routage_ll.adb
+++ Routeur_Cache/test_routage_ll.adb
@@ -12,9 +12,9 @@ procedure test_routage_LL is
 
 
 
-    procedure test_Mise_A_Jour_Cache is
+    procedure test_Mise_A_Jour_Cache_LRU is
     begin
-        put("test de la procedure Mise_A_Jour_Cache"); New_Line;
+        put("test de la procedure Mise_A_Jour_Cache_LRU"); New_Line;
 
 
         put("print Remplissage de la table de routage, Etape 1"); New_Line;
@@ -54,11 +54,51 @@ procedure test_routage_LL is
 
         Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.128.10.10")), To_Unbounded_String("LFU"));
 
-        put("Fin des tests pour Ajouter_Element."); New_Line;
+        put("Fin des tests pour Ajouter_Element et Mise_A_Jour_Cache en politique LFU."); New_Line;
+
     exception
         when INTERFACE_NON_TROUVE =>
             put("L'élément le moins utilisé du cache a bien été supprimé");
-    end test_Mise_A_Jour_Cache;
+    end test_Mise_A_Jour_Cache_LRU;
+
+
+
+
+    procedure test_Mise_A_Jour_Cache_FIFO is
+    begin
+        put("test de la procedure Mise_A_Jour_Cache_FIFO"); New_Line;
+
+
+
+
+        put("Remplissage de cache"); New_Line;
+        put("Etape 1"); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.8")) ,3, To_Unbounded_String("FIFO"),0,  To_Unbounded_String("eth2"), Table);
+        Afficher_Cache(Cache, To_Unbounded_String("FIFO")); New_Line; New_Line;
+
+        put("Etape 2"); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.5")) ,3, To_Unbounded_String("FIFO"),2,  To_Unbounded_String("eth3"), Table);
+        Afficher_Cache(Cache, To_Unbounded_String("FIFO")); New_Line; New_Line;
+
+        put("Etape 3"); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.128.10.10")) ,3, To_Unbounded_String("FIFO"),1,  To_Unbounded_String("eth1"), Table);
+        Afficher_Cache(Cache, To_Unbounded_String("FIFO")); New_Line; New_Line;
+
+
+
+        put("Etape 4: Depassement de la taille max du cache et suppression de l'élément d'interface 'eth2' "); New_Line;
+        Mise_A_Jour_Cache(Cache, Texte_Vers_IP(To_Unbounded_String("147.127.10.8")) ,3, To_Unbounded_String("FIFO"),3,  To_Unbounded_String("eth3"), Table);
+        Afficher_Cache(Cache, To_Unbounded_String("FIFO")); New_Line; New_Line;
+
+
+        Trouver_Interface_Cache(Interface_Trouvee, Cache, Texte_Vers_IP(To_Unbounded_String("147.127.127.8")), To_Unbounded_String("FIFO"));
+
+        put("Fin des tests pour Ajouter_Element et Mise_A_Jour_Cache en politique FIFO."); New_Line;
+
+    exception
+        when INTERFACE_NON_TROUVE =>
+            put("L'élément le moins utilisé du cache a bien été supprimé");
+    end test_Mise_A_Jour_Cache_FIFO;
 
 
     procedure test_Trouver_Interface_Cache is
@@ -80,7 +120,8 @@ begin
     Initialiser_Table_Vide(Table);
     Initialiser_Table_Vide(Cache);
 
-    test_Mise_A_Jour_Cache;
+    test_Mise_A_Jour_Cache_FIFO;
+    test_Mise_A_Jour_Cache_LRU;
     test_Trouver_Interface_Cache;
 
     New_Line;
